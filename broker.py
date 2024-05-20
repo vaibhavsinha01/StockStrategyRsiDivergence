49006c7d0c6d99322d60d6630d363c64ead7924c
Vaibhav Sinha <vaibhavrajsinha099@gmail.com>
2024-05-20 17:15:41 +0530

    fourth iteration

--- broker.py
+++ broker.py
@@ -8,19 +8,14 @@ class ByBitBroker:
         self.balance = balance
         self.stocks = {}
 
-    def login(user, password, k, secr):
+    def login(self, user, password, k, secr):
         if user == login and password == password and k == key and secr == secret:
             print("Successfully logged in")
         else:
             print("Login unsuccessful")
     
     def place_order(self, quantity, symbol):
-        header = {
-            'X-BAPI-API-KEY': key,
-            'X-BAPI-TIMESTAMP': '1716201412259', 
-            'X-BAPI-RECV-WINDOW': '20000',
-            'X-BAPI-SIGN': 'd97c47798adf11dffcf16c292d56fc1e594a5a4f45a4a0137b3ba384c0ac026a'
-        }
+        header = self.get_default_header()
         payload = {
             "category": "linear",
             "symbol": symbol,
@@ -30,12 +25,7 @@ class ByBitBroker:
         print(response.text)  
     
     def cancel_order(self, order_id):
-        header = {
-            'X-BAPI-API-KEY': key,
-            'X-BAPI-TIMESTAMP': '1716201412259',  
-            'X-BAPI-RECV-WINDOW': '20000',
-            'X-BAPI-SIGN': 'd97c47798adf11dffcf16c292d56fc1e594a5a4f45a4a0137b3ba384c0ac026a'
-        }
+        header = self.get_default_header()
         payload = {
             "orderId": order_id,
         }
@@ -44,3 +34,14 @@ class ByBitBroker:
 
     def get_stocks(self):
         return self.stocks
+    
+    def get_balance(self):
+        return self.balance
+
+    def get_default_header(self):
+        return {
+            'X-BAPI-API-KEY': key,
+            'X-BAPI-TIMESTAMP': '1716201412259', 
+            'X-BAPI-RECV-WINDOW': '20000',
+            'X-BAPI-SIGN': 'd97c47798adf11dffcf16c292d56fc1e594a5a4f45a4a0137b3ba384c0ac026a'
+        }
